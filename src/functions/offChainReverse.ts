86228eb784a9ea215349be73441f4d74e7c4756d
Ghadi Mhawej <ghadimhawej@outlook.com>
2024-02-26 11:08:00 +0200

    fix: fix off-chain printed name

--- src/functions/offChainReverse.ts
+++ src/functions/offChainReverse.ts
@@ -4,29 +4,30 @@ import { resolverSelector, nameSelector } from "../utils/abis";
 import { arrayify } from "@ethersproject/bytes";
 
 const offChainReverse = async (
-    nameHash: string,
-    resolverAddress: string,
-    provider: ethers.Provider
+  nameHash: string,
+  resolverAddress: string,
+  provider: ethers.Provider
 ): Promise<string> => {
   const dnsEncodedName: string = await dnsName(nameHash);
 
   // Get method selector (first 4 bytes of the Keccak-256 hash of the function signature)
   const resolverFunctionSelector: string = ethers
-      .keccak256(ethers.toUtf8Bytes(resolverSelector))
-      .slice(0, 10); // Take only first 4 bytes
+    .keccak256(ethers.toUtf8Bytes(resolverSelector))
+    .slice(0, 10); // Take only first 4 bytes
   const nameFunctionSelector: string = ethers
-      .keccak256(ethers.toUtf8Bytes(nameSelector))
-      .slice(0, 10); // Take only first 4 bytes
+    .keccak256(ethers.toUtf8Bytes(nameSelector))
+    .slice(0, 10); // Take only first 4 bytes
 
   const abiCoder: ethers.AbiCoder = new ethers.AbiCoder();
 
   const encodedNameCallData: string = abiCoder.encode(["bytes32"], [nameHash]);
 
-  const nameFunctionData: string = nameFunctionSelector + encodedNameCallData.slice(2);
+  const nameFunctionData: string =
+    nameFunctionSelector + encodedNameCallData.slice(2);
 
   const txData: string = abiCoder.encode(
-      ["bytes", "bytes"],
-      [arrayify(dnsEncodedName), arrayify(nameFunctionData)]
+    ["bytes", "bytes"],
+    [arrayify(dnsEncodedName), arrayify(nameFunctionData)]
   );
 
   const finalTxData: string = resolverFunctionSelector + txData.slice(2);
@@ -39,10 +40,15 @@ const offChainReverse = async (
 
   const response: string = await provider.call(tx);
 
-  const decodedData = abiCoder.decode(['string'], response);
+  const decodedData = abiCoder.decode(["string"], response);
   const name = decodedData[0];
 
-  return name;
+  const cleanedName = name
+    .replace(/[\u200B-\u200D\uFEFF]/g, "") // Remove specific known non-visible characters
+    .replace(/[\x00-\x20\x7F-\xA0]/g, "") // Remove a broad range of non-printable/control characters
+    .trim(); // Lastly, trim for good measure
+
+  return cleanedName;
 };
 
 export { offChainReverse };
